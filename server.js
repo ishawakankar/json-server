e857f008dd8fc8f56a657c4a304b2eedd48a0628
ishawakankar <iwakankar@hotmail.com>
2019-01-29 12:19:32 +0530

    swagger documentation added

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+ 
 app.listen(8000, () => {
     console.log('App listening on port 8000')
 })
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.send('Welcome')
@@ -13,7 +17,7 @@ app.get('/', (req, res) => {
 app.get('/api/v1/queryResponse', (req, res) => {
     axios('http://localhost:3000/queryResponse').then(res => {
         console.log(res.data)
-        // res.send(res.data)
+        // res.send(res.data.status)
     })
 })
 
